f614281bc6fbcd799c6dc76e2b27857907e5c8c5
Žan Hertiš <zanhertis@gmail.com>
2025-03-22 20:27:32 +0100

    Another scenario, added conversion from rad to deg where necessary, added a switch to lateral controller so if steering rate is NaN due to missing input parameters (lane not detected) it will instead output 0 (unchanged angle).

--- SimulationModel/mpc_model_script.m
+++ SimulationModel/mpc_model_script.m
@@ -9,15 +9,15 @@ mpc3.Model.Nominal.U = 0;
 mpc3.Model.Nominal.Y = [0;0];
 %% specify scale factors for inputs and outputs
 mpc3.MV(1).ScaleFactor = 1;
-mpc3.OV(1).ScaleFactor = 10;
+mpc3.OV(1).ScaleFactor = 0.5;
 mpc3.OV(2).ScaleFactor = 1;
 %% specify constraints for MV and MV Rate
 mpc3.MV(1).Min = -0.20;
 mpc3.MV(1).Max = 0.20;
 %% specify weights
-mpc3.Weights.MV = 1;
-mpc3.Weights.MVRate = 0.2;
-mpc3.Weights.OV = [0.6 0.4];
+mpc3.Weights.MV = 0;
+mpc3.Weights.MVRate = 0.1;
+mpc3.Weights.OV = [1 1];
 mpc3.Weights.ECR = 1000;
 
 disp(mpc3.PredictionHorizon)
