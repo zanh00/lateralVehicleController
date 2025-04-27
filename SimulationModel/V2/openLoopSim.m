7394c76735b70a4519ff361b66bd7b7d1a201ff0
Žan Hertiš <zanhertis@gmail.com>
2025-04-27 19:22:47 +0200

    clean up

--- SimulationModel/V2/openLoopSim.m
+++
@@ -1,8 +0,0 @@
-ss_gain = cloffset(mpc3);
-disp(ss_gain);
-
-%%
-T = 100;
-r = [0 0 0;
-     0.5 0 0];
-sim(mpc1, T, r);
--- SimulationModel/openLoopSim.m
+++ SimulationModel/openLoopSim.m
@@ -2,7 +2,7 @@ ss_gain = cloffset(mpc3);
 disp(ss_gain);
 
 %%
-T = 150;
-r = [0 0;
-     1 0];
-sim(mpc1_1, T, r);
+T = 100;
+r = [0 0 0;
+     0.5 0 0];
+sim(mpc1, T, r);
